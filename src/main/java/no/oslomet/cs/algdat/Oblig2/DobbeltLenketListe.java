4574fc3446df2f3bba155ab1e2748dad5f4a14c6
Adrian <adrianwasheim@gmail.com>
2022-10-11 22:44:34 +0200

    Lagde boolean inneholder

--- src/main/java/no/oslomet/cs/algdat/Oblig2/DobbeltLenketListe.java
+++ src/main/java/no/oslomet/cs/algdat/Oblig2/DobbeltLenketListe.java
@@ -140,7 +140,11 @@ public class DobbeltLenketListe<T> implements Liste<T> {
 
     @Override
     public boolean inneholder(T verdi) {
-        throw new UnsupportedOperationException();
+        if (indeksTil(verdi) == -1){
+            return false;
+        } else {
+            return true;
+        }
     }
 
     //Oppgave 3
